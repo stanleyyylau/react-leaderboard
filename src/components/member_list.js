60d140ea55c6400fe9801019708ffa6405bf7d67
Stanley <stanleyyylau@gmail.com>
2016-09-25 23:38:19 +0800

    add nice loading effect, it's even awesomer

--- src/components/member_list.js
+++ src/components/member_list.js
@@ -3,6 +3,15 @@ import MemberListHeader from './member_list_header';
 import MemberListItem from './member_list_item';
 
 const MemberList = (props) => {
+  if(props.data.length === 0){
+    return (
+      <ul className = "member-list">
+        <MemberListHeader />
+        <h3>Loading...</h3>
+      </ul>
+    );
+  }
+
   const data = props.data;
   var keyNum = 0;
   const allList = data.map((value, index) => {
